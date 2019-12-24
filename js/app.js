03fa6f296f2af423d8290b94bc67e42ef3e08733
Markcreator <markmediahorn@hotmail.com>
2019-12-24 12:52:18 +0100

    Implemented anomaly

--- js/app.js
+++ js/app.js
@@ -195,9 +195,11 @@ $(function() {
 		}
 	};
 	var worldState;
+	var disc;
 	
 	// Timers
-	for(var acolyte in acolytes) {
+	// TODO turn into timed anomaly discovery
+	/*for(var acolyte in acolytes) {
 		var json = acolytes[acolyte];
 		var name = json.name;
 		var mods = json.mods;
@@ -221,7 +223,7 @@ $(function() {
 			
 			startTimer($("#" + name + "-timer"), json.arrival, $("#" + name + "-timer-card"));
 		}
-	}
+	}*/
 	
 	// Start update loop
 	update();
@@ -277,142 +279,39 @@ $(function() {
 		}		
 	}
 	
-	// Hidden Acolyte card builder
-	function hiddenAcolyte(name) {
+	// Function to render anomaly cards
+	function render() {
+		$("#anomaly").empty();
+		var tmpState = worldState.Tmp;
+		var currentDisc = typeof tmpState.sfn !== 'undefined';
+		var location = disc ? escapeHtml(nodes["CrewBattleNode" + tmpState.sfn].value) : getLangText("unknown");
+		
+		if(disc != currentDisc) {
+			disc = currentDisc;
+			
+			if($("#sounds").hasClass("green")) {
+				audio.play();
+			}
+			if($("#notifications").hasClass("green")) {
+				notifyAnomaly(disc, location);
+			}
+		}
+			
 		var output = [];
-		output.push('<div id="acolyte-' + name + '" class="card grey lighten-4 horizontal hoverable">');
-
-		output.push('<div class="card-content flow-text">');
-		output.push("	<b>" + name.toUpperCase() + "</b>	<a id='show-" + name + "' name='" + name + "' class='pointer'>" + getLangText("show") + "</a>");
+		output.push('<div id="anomaly" class="card grey lighten-4 horizontal hoverable">');
+		output.push('	<div class="card-image">');
+		output.push('		<img src="img/anomaly.jpg">');
+		output.push('	</div>');
+		output.push('	<div class="card-stacked">');
+		output.push('		<div class="card-content flow-text">');
+		output.push("			<b>Sentient Anomaly</b>");
+		output.push("			<b>" + getLangText("location") + "</b> " + location);
+		output.push("			<br/>");
+		output.push('		</div>');
+		output.push('	</div>');
 		output.push('</div>');
 		
-		return output.join("");
-	}
-	
-	// Function to render all Acolyte cards
-	function render() {
-		$("#acolytes").empty();
-		currentAcolytes = {};
-		
-		var acolyteList = worldState.PersistentEnemies;
-		// Test data
-		if(useTestData) {
-			acolyteList = [{
-				Icon: "/DuellistAcolyte.png",
-				Discovered: Math.random() >= 0.5,
-				HealthPercent: 0.789,
-				LastDiscoveredLocation: "SolNode23"
-			},
-			{
-				Icon: "/HeavyAcolyte.png",
-				Discovered: Math.random() >= 0.5,
-				HealthPercent: 0.123,
-				LastDiscoveredLocation: "SolNode24"
-			},
-			{
-				Icon: "/StrikerAcolyte.png",
-				Discovered: false,
-				Region: 4,
-				HealthPercent: 0.123,
-				LastDiscoveredLocation: "SolNode24"
-			}];
-		}
-
-		for(var i = 0; i < acolyteList.length; i++) {
-			var aco = acolyteList[i];
-			
-			var acoName = aco.Icon.split("/")[aco.Icon.split("/").length-1].split(".png")[0];
-			var name = acolytes[acoName].name;
-			var disc = aco.Discovered;
-			var region = aco.Region;
-			var health = aco.HealthPercent;
-			var mods = acolytes[acoName].mods;
-			var location = disc ? escapeHtml(nodes[aco.LastDiscoveredLocation].value + " [" + nodes[aco.LastDiscoveredLocation].type + "]") : ((region && regions[region] != "") ? getLangText("signalon") + " " + regions[region] : getLangText("unknown"));
-			
-			if(acolytes[acoName].disc != disc) {
-				acolytes[acoName].disc = disc;
-				
-				if($("#sounds").hasClass("green")) {
-					if(!localStorage["hide-" + name]) {
-						audio.play();
-					}
-				}
-				if($("#notifications").hasClass("green")) {
-					if(!localStorage["hide-" + name]) {
-						notifyAcolyte(acoName, name, disc, location);
-					}
-				}
-			}
-				
-			var output = [];
-			output.push('<div id="acolyte-' + name + '" class="card grey lighten-4 horizontal hoverable">');
-			output.push('	<div class="card-image">');
-			output.push('		<img src="img/' + acoName + '.png">');
-			output.push('	</div>');
-			output.push('	<div class="card-stacked">');
-			output.push('		<div class="card-content flow-text">');
-			output.push("			<b>" + name.toUpperCase() + "</b>	<a id='hide-" + name + "' name='" + name + "' class='pointer'>" + getLangText("hide") + "</a>	<a class='right' target='_blank' href='http://warframe.wikia.com/wiki/" + name + "'>" + getLangText("wiki") + "</a>");
-			output.push('			<div class="progress grey darken-2"> <div class="determinate health" style="width: ' + (health * 100) + '%"></div> </div>');
-			output.push("			<span class='red-text'><b>" + getLangText("health") + "</b> " + (health * 100).toFixed(2) + "%</span>");
-			output.push("			<br/>");
-			output.push("			<b>" + getLangText("location") + "</b> " + location);
-			output.push("			<br/>");
-			output.push("			<a class='dropdown-button btn waves-effect waves-light grey darken-3 grey-text right' data-beloworigin='true' data-activates='dropdown-" + name + "'>" + getLangText("drops") + "</a>");
-			output.push("			<ul id='dropdown-" + name + "' class='dropdown-content'>");
-			var x = 0;
-			for(var x = 0; x < mods.length; x++) {
-			output.push("				<li><a target='_blank' href='http://warframe.wikia.com/wiki/" + mods[x].split(" (")[0].replace(" ", "_") + "' class='grey lighten-4 grey-text text-darken-1'>" + mods[x] + "</a></li>");
-			}
-			output.push("			</ul");
-			output.push('		</div>');
-			output.push('	</div>');
-			output.push('</div>');
-			
-			currentAcolytes[name] = output.join("");
-		}
-		
-		if(acolyteList.length == 0) {
-			var output = [];
-			output.push('<div class="card grey lighten-4 hoverable">');
-			output.push('	<div class="card-content flow-text green-text">');
-			output.push('		' + getLangText("eventstarting"));
-			output.push('	</div>');
-			output.push('</div>');
-			
-			$("#acolytes").append(output.join(""));
-			
-		} else {
-			function show() {
-				var name = $(this).attr("name");
-							
-				delete localStorage["hide-" + name];
-				render();
-			}
-			
-			function hide() {
-				var name = $(this).attr("name");
-							
-				localStorage["hide-" + name] = true;
-				render();
-			}
-			
-			for(var x = 0; x < acolyteOrder.length; x++) {
-				var name = acolyteOrder[x];
-				
-				if(typeof(currentAcolytes[name]) != 'undefined') {
-					if(!localStorage["hide-" + name]) {
-						$("#acolytes").append(currentAcolytes[name]);
-					
-						$("#hide-" + name).click(hide);
-						
-					} else {
-						$("#acolytes").append(hiddenAcolyte(name));
-						
-						$("#show-" + name).click(show);
-					}
-				}
-			}
-		}
+		$("#anomaly").append(output.join(""));
 		
 		$('.dropdown-button').dropdown({
 			constrainWidth: false
@@ -442,11 +341,11 @@ $(function() {
 	}
 	
 	// Notification behaviour
-	function notifyAcolyte(acoName, name, disc, location) {
-		var title = "Acolyte Tracker";
+	function notifyAnomaly(disc, location) {
+		var title = "Anomaly Tracker";
 		var options = {
-			icon: 'img/' + acoName + '.png',
-			body: name.toUpperCase() + getLangText("acolytelocationupdate") + "\n" + location
+			icon: 'img/anomaly.jpg',
+			body: "Sentient Anomaly" + getLangText("acolytelocationupdate") + "\n" + location
 		};
 		
 		if (!("Notification" in window)) {
